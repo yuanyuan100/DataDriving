cac6691c48fdc8e7054fd6fec196ba99edc0c3c9
yuanyuan100 <yuanyuan_pyy@163.com>
2017-06-18 15:55:56 +0800

    [modify]准备例子

--- DataDrivingDemo/Source/DDNetParse.h
+++ DataDrivingDemo/Source/DDNetParse.h
@@ -8,6 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface DDNetParse : NSObject
+
+/**
+ 请使用类别重写该方法，以提供默认的json转model
+
+ @param model model对象
+ @param json json
+ */
 + (void)dd_defaultParse:(id)model json:(id)json;
 @end
+
+NS_ASSUME_NONNULL_END
--- DataDrivingDemo/Source/DDNetParse.m
+++ DataDrivingDemo/Source/DDNetParse.m
@@ -8,8 +8,12 @@
 
 #import "DDNetParse.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
 @implementation DDNetParse
 + (void)dd_defaultParse:(id)model json:(id)json {
-    NSAssert(NO, @"需要提供默认解析方案，请使用分类重写该方法");
+    NSAssert(NO, @"需要提供默认解析方案，请使用类别重写该方法");
 }
 @end
+
+NS_ASSUME_NONNULL_END
